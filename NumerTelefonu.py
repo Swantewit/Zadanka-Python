974a2d672c8ffdadbe9d9141ef8eb034fdc8c9f4
Swantewit <feniks@Kubas-MacBook-Air.local>
2020-08-08 22:46:59 +0200

    second commit

--- NumerTelefonu.py
+++ NumerTelefonu.py
@@ -2,15 +2,20 @@ import re
 
 moList = []
 phoneList = []
-phoneNumRegex1 = re.compile(r'(\d\d\d) \d\d \d\d \d\d\d')
-phoneNumRegex2 = re.compile(r'(\d\d) \d\d \d\d\d \d\d')
+phoneNumRegex1 = re.compile(r'(\d\d\d)\s\d\d\s\d\d\s\d\d\d')
+phoneNumRegex2 = re.compile(r'(\d\d)\s\d\d\s\d\d\d\s\d\d')
 phoneNumRegex3 = re.compile(r'\d\d\d\d\d\d\d\d\d')
 phoneNumRegex4 = re.compile(r'\d\d\d\d\d\d\d')
-phoneNumRegex5 = re.compile(r'00\d\d \d\d\d\d\d\d\d\d\d')
-phoneNumRegex6 = re.compile(r'\d\d - \d\d\d - \d\d - \d\d')
-phoneNumRegex7 = re.compile(r'\d\d\d - \d\d - \d\d')
+phoneNumRegex5 = re.compile(r'0048 \d\d\d \d\d\d \d\d\d')
+phoneNumRegex6 = re.compile(r'\d\d\s-\s\d\d\d\s-\s\d\d\s-\s\d\d')
+phoneNumRegex7 = re.compile(r'\d\d\d\s-\s\d\d\s-\s\d\d')
 phoneNumRegex8 = re.compile(r'\d\d-\d\d\d-\d\d-\d\d')
-phoneNumRegex9 = re.compile(r'\d\d\d-\d\d -\d\d')
+phoneNumRegex9 = re.compile(r'\d\d\d-\d\d-\d\d')
+phoneNumRegex10 = re.compile(r'\+48\s\d\d\d\s\d\d\d\s\d\d\d')
+phoneNumRegex11 = re.compile(r'0048\d\d\d\d\d\d\d\d\d')
+phoneNumRegex12 = re.compile(r'\+48\d\d\d\d\d\d\d\d\d')
+phoneNumRegex13 = re.compile(r'\d\d\d\s\d\d\s\d\d')
+
 print('Witamy w programie "Znajdź polski numer telefonu?"\nWpisz tekst, by sprawdzić, czy zawiera polski numer telefonu\nWpisz "Wyjdź", aby zakończyć...')
 
 
@@ -30,10 +35,14 @@ def toNumerTelefonu(numer):
         mo7 = phoneNumRegex7.search(n)
         mo8 = phoneNumRegex8.search(n)
         mo9 = phoneNumRegex9.search(n)
-        moList.extend([mo1, mo2, mo3, mo4, mo5, mo6, mo7, mo8, mo9])
+        mo10 = phoneNumRegex10.search(n)
+        mo11 = phoneNumRegex11.search(n)
+        mo12 = phoneNumRegex12.search(n)
+        mo13 = phoneNumRegex13.search(n)
+        moList.extend([mo1, mo2, mo3, mo4, mo5, mo6, mo7, mo8, mo9, mo10, mo11, mo12, mo13])
         filterNumber(moList)
         for i in phoneList:
-                print('Phone number found: ' + i.group())
+                print('Znaleziono numer telefonu: ' + i.group())
 
 
 while True:
