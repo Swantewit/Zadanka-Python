455982804cb6c5d192504d70d02343a5c9b79d33
Swantewit <feniks@Kubas-MacBook-Air.local>
2020-08-10 20:12:47 +0200

    fourth commit

--- NumerTelefonu.py
+++ NumerTelefonu.py
@@ -2,19 +2,18 @@ import re
 
 moList = []
 phoneList = []
-phoneNumRegex1 = re.compile(r'\(\d\d\d\)\s\d\d\s\d\d\s\d\d\d')
-phoneNumRegex2 = re.compile(r'\(\d\d\)\s\d\d\s\d\d\d\s\d\d')
-phoneNumRegex3 = re.compile(r'\d\d\d\d\d\d\d\d\d')
-phoneNumRegex4 = re.compile(r'\d\d\d\d\d\d\d')
-phoneNumRegex5 = re.compile(r'0048 \d\d\d \d\d\d \d\d\d')
-phoneNumRegex6 = re.compile(r'\d\d\s-\s\d\d\d\s-\s\d\d\s-\s\d\d')
-phoneNumRegex7 = re.compile(r'\d\d\d\s-\s\d\d\s-\s\d\d')
-phoneNumRegex8 = re.compile(r'\d\d-\d\d\d-\d\d-\d\d')
+phoneNumRegex1 = re.compile(r'(\(\d\d\d\)\s)?\d\d\s\d\d\s\d\d\d')
+phoneNumRegex2 = re.compile(r'(\(\d\d\)\s)?\d\d\s\d\d\d\s\d\d')
+phoneNumRegex3 = re.compile(r'(\d\d)?\d\d\d\d\d\d\d')
+phoneNumRegex4 = re.compile(r'(\(\d\d\)\s)?\d\d\d\s\d\d\s\d\d')
+phoneNumRegex5 = re.compile(r'(0048\s)?\d\d\d\s\d\d\d\s\d\d\d')
+phoneNumRegex6 = re.compile(r'(\d\d\s-\s)?\d\d\d\s-\s\d\d\s-\s\d\d')
+phoneNumRegex8 = re.compile(r'(\d\d-)?\d\d\d-\d\d-\d\d')
 phoneNumRegex9 = re.compile(r'\d\d\d-\d\d-\d\d')
-phoneNumRegex10 = re.compile(r'\+48\s\d\d\d\s\d\d\d\s\d\d\d')
+phoneNumRegex10 = re.compile(r'(\+48\s)?\d\d\d\s\d\d\d\s\d\d\d')
 phoneNumRegex11 = re.compile(r'0048\d\d\d\d\d\d\d\d\d')
 phoneNumRegex12 = re.compile(r'\+48\d\d\d\d\d\d\d\d\d')
-phoneNumRegex13 = re.compile(r'\d\d\d\s\d\d\s\d\d')
+
 
 print('Witamy w programie "Znajdź polski numer telefonu?"\nWpisz tekst, by sprawdzić, czy zawiera polski numer telefonu\nWpisz "Wyjdź", aby zakończyć...')
 
@@ -32,14 +31,12 @@ def toNumerTelefonu(numer):
         mo4 = phoneNumRegex4.search(n)
         mo5 = phoneNumRegex5.search(n)
         mo6 = phoneNumRegex6.search(n)
-        mo7 = phoneNumRegex7.search(n)
         mo8 = phoneNumRegex8.search(n)
         mo9 = phoneNumRegex9.search(n)
         mo10 = phoneNumRegex10.search(n)
         mo11 = phoneNumRegex11.search(n)
         mo12 = phoneNumRegex12.search(n)
-        mo13 = phoneNumRegex13.search(n)
-        moList.extend([mo1, mo2, mo3, mo4, mo5, mo6, mo7, mo8, mo9, mo10, mo11, mo12, mo13])
+        moList.extend([mo1, mo2, mo3, mo4, mo5, mo6, mo8, mo9, mo10, mo11, mo12])
         filterNumber(moList)
         for i in phoneList:
                 print('Znaleziono numer telefonu: ' + i.group())
