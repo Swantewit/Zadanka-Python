f1849b6de529873cd1077be017583c98e755df66
Swantewit <feniks@Kubas-MacBook-Air.local>
2020-08-13 20:40:16 +0200

    first commit

---
+++ NrTelFindall.py
@@ -0,0 +1,43 @@
+import re
+
+moList = []
+phoneList = []
+phoneRegex = [re.compile(r'\(\d\d\d\)\s\d\d\s\d\d\s\d\d\d')
+,re.compile(r'\(\d\d\)\s\d\d\s\d\d\d\s\d\d')
+,re.compile(r'\d\d\d\d\d\d\d\d\d')
+,re.compile(r'\d\d\d\d\d\d\d')
+,re.compile(r'\(\d\d\)\s\d\d\d\s\d\d\s\d\d')
+,re.compile(r'\d\d\d\s\d\d\s\d\d')
+,re.compile(r'0048\s\d\d\d\s\d\d\d\s\d\d\d')
+,re.compile(r'\d\d\s-\s\d\d\d\s-\s\d\d\s-\s\d\d')
+,re.compile(r'\d\d-\d\d\d-\d\d-\d\d')
+,re.compile(r'\d\d\d-\d\d-\d\d')
+,re.compile(r'\+48\s\d\d\d\s\d\d\d\s\d\d\d')
+,re.compile(r'0048\d\d\d\d\d\d\d\d\d')
+,re.compile(r'\+48\d\d\d\d\d\d\d\d\d')]
+
+
+print('Witamy w programie "Znajdź polski numer telefonu?"\nWpisz tekst, by sprawdzić, czy zawiera polski numer telefonu\nWpisz "Wyjdź", aby zakończyć...')
+
+
+def filterNumber(numberList):
+        for i in numberList:
+                if i != []:
+                        phoneList.append(i)
+        
+
+def toNumerTelefonu(numer):
+        for i in phoneRegex:
+            moList.append(i.findall(numer))
+        filterNumber(moList)
+        print('Znaleziono następujące numery' + str(phoneList))
+        
+
+while True:
+    n = input()
+
+    if n == 'Wyjdź':
+            print('Dziękujemy za skorzystanie z programu.')
+            break
+    else:
+        toNumerTelefonu(n)
